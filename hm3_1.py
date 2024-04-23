160840991d5aa686ca88f4cb78efe2b7a2fc63fe
Viktoria-I <viktoria@nitra.dev>
2024-04-23 23:40:52 +0300

    hm_3

---
+++ hm3_1.py
@@ -0,0 +1,44 @@
+import os
+import threading
+import shutil
+import logging
+
+def sort_files(source_folder, target_folder):
+
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info(f"Start sorting files from '{source_folder}' to '{target_folder}'")
+  
+    if not os.path.exists(source_folder):
+         logging.error(f"Error: Folder '{source_folder}' not found")
+        
+    
+    for root, _, files in os.walk(source_folder):
+        for file in files:
+            file_path = os.path.join(root, file)
+            _, file_ext = os.path.splitext(file)
+
+            ext_dir = os.path.join(target_folder, file_ext[1:])
+            if not os.path.exists(ext_dir):
+                try:
+                    os.makedirs(ext_dir)
+                except OSError as e:
+                    logging.error(f"Error creating folder '{ext_dir}': {e}")
+                    continue
+
+            shutil.copy2(file_path, ext_dir)
+
+    logging.info("Files were sorted.")
+
+if __name__ == "__main__":
+    source_folder = input("Input source folder path: ")
+    target_folder = input("Input target folder path (default: 'E:\dist'): ") or "E:\dist"
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    else:
+        print(f"Error: Folder '{target_folder}' is exist. Please enter another folder name.")
+
+    thread = threading.Thread(target=sort_files, args=(source_folder, target_folder))
+    thread.start()
+    thread.join()
---
+++ hm3_2.py
@@ -0,0 +1,29 @@
+import time
+import multiprocessing as mp
+from concurrent.futures import ProcessPoolExecutor
+
+def factorize(*numbers):
+    
+    res_numbers = []
+    
+    for number in numbers:
+        res_number = []
+        for i in range(1, number + 1):
+            if number % i == 0:
+                res_number.append(i)
+        res_numbers.append(res_number)
+    return res_numbers
+
+
+
+if __name__ == '__main__':
+    print("Start without multiprocessing")
+    start = time.time()
+    a, b, c, d = factorize(128, 255, 99999, 10651060)
+    print("Time without multiprocessing: ", time.time() - start)
+    print("Start with multiprocessing")
+    start = time.time()
+    with ProcessPoolExecutor(mp.cpu_count()) as executor:
+        a, b, c, d = executor.map(factorize, (128, 255, 99999, 10651060))
+    print("Time with multiprocessing: ", time.time() - start)
+    print(f"128: {a}\n255: {b}\n99999: {c}\n10651060: {d}")
